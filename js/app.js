5993bd3dfdede1c29be36f129ae9182c7b0ac416
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-02-17 14:13:24 +0600

    saving amount added

--- js/app.js
+++ js/app.js
@@ -8,21 +8,37 @@ function totalExpense(){
     const foodInputValue=parseInt(foodInput.value); 
     const rentInputValue=parseInt(rentInput.value); 
     const clothesInputValue=parseInt(clothesInput.value);
-    return foodInputValue+rentInputValue+clothesInputValue;
+    const totalExp= foodInputValue+rentInputValue+clothesInputValue;
+    const setTotalExpense=document.getElementById('total-expenses');
+    setTotalExpense.innerText=totalExp;
+    calculateBalance(totalExp);
 }
 
 //balance calculation
-function calculateBalance(totalExp){
+function calculateBalance(totalExp){ 
     const incomeInputValue=parseInt(incomeInput.value);
-    return incomeInputValue-totalExp;
+    const balance= incomeInputValue-totalExp;
+    const setBalance=document.getElementById('balance');
+    setBalance.innerText=balance;
+}
+
+//saving calculation
+function calculateSaving(){
+    const incomeInputValue=parseInt(incomeInput.value);
+    const saveInput=document.getElementById('save');
+    const saveInputValue=parseInt(saveInput.value);
+    const savingAmount=incomeInputValue*(saveInputValue/100);
+    const setSavingAmount=document.getElementById('saving-amount');
+    setSavingAmount.innerText=savingAmount;
 }
 
 // calculate button 
 document.getElementById('btn-calculate').addEventListener('click',function(){
-    const totalExp=totalExpense();
-    const setTotalExpense=document.getElementById('total-expenses');
-    setTotalExpense.innerText=totalExp;
-    const balance=calculateBalance(totalExp);
-    const setBalance=document.getElementById('balance');
-    setBalance.innerText=balance;
+    totalExpense();   
+});
+
+// savings buttons
+document.getElementById('btn-saving').addEventListener('click',function(){
+    calculateSaving();
+    
 });
