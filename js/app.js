7639a18e44d86a881650ccb7c61461e875623b3b
rayhan1650 <nm.rayhan.1650@gmail.com>
2022-02-17 09:33:46 +0600

    balance function added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const foodInput=document.getElementById('food');
 const rentInput=document.getElementById('rent');
 const clothesInput=document.getElementById('clothes');
+const incomeInput=document.getElementById('income');
 
 // expenses function 
 function totalExpense(){
@@ -10,9 +11,18 @@ function totalExpense(){
     return foodInputValue+rentInputValue+clothesInputValue;
 }
 
+//balance calculation
+function calculateBalance(totalExp){
+    const incomeInputValue=parseInt(incomeInput.value);
+    return incomeInputValue-totalExp;
+}
+
 // calculate button 
 document.getElementById('btn-calculate').addEventListener('click',function(){
     const totalExp=totalExpense();
     const setTotalExpense=document.getElementById('total-expenses');
     setTotalExpense.innerText=totalExp;
+    const balance=calculateBalance(totalExp);
+    const setBalance=document.getElementById('balance');
+    setBalance.innerText=balance;
 });
